b9be3ad14163531c94af23710fd0a65dac78daef
David Pitman <david.pitman@cloudouble.com>
2023-11-23 16:51:38 +1000

    creating clean version leveraging packages

--- packages/main.js
+++ packages/main.js
@@ -1,5 +1,12 @@
 export default {
     variables: {
-        test: 123
+        sources: {
+            localStorage: '`localStorage.setItem(todos-elementhtml)!getItem(todos-elementhtml)`'
+        },
+        updates: {
+            add: '($t:=$trim($); $length($t)>0 ? {"id": $id, "title": $t, "completed": false} : null)',
+            mergeByIndex: '$filter($map($value, function($t, $i) { $i = $.index ? ($.update ? $merge([$t, $.update]) : false) : $t }), function($v) { $v })',
+            toggleCompleteAll: '$map($value, function($t) { $merge([$t, {"completed": $}])})',
+        }
     }
 }
