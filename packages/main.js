4cbeaec4d80c9446906cf517f4bf7708571ee32f
David Pitman <david.pitman@cloudouble.com>
2023-12-06 23:25:23 +1000

    re-building

--- packages/main.js
+++ packages/main.js
@@ -3,12 +3,6 @@ export default {
         storage: {
             local: '`localStorage.setItem(todos-elementhtml) -> getItem(todos-elementhtml)`',
             session: '`sessionStorage.setItem(todos-elementhtml) -> getItem(todos-elementhtml)`'
-        },
-        defaultCell: 'todos',
-        defaults: {
-            router: ":|#router",
-            transform: '($getCell("todos"))'
-        },
-        testFunc: (arg1, arg2) => { console.log('main.js: line 8', arg1, arg2); return arg1 + arg2; }
+        }
     }
 }
