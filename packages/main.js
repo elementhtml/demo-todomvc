7d610e621860526024f107701113019379e0f977
David Pitman <david.pitman@cloudouble.com>
2024-01-04 15:42:22 +1000

    re-built on beta release

--- packages/main.js
+++
@@ -1,8 +0,0 @@
-export default {
-    variables: {
-        storage: {
-            local: '`localStorage.setItem(todos-elementhtml) -> getItem(todos-elementhtml)`',
-            session: '`sessionStorage.setItem(todos-elementhtml) -> getItem(todos-elementhtml)`'
-        }
-    }
-}
